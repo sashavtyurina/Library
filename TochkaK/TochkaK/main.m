e06d22ec14e1a20994ded478c32602f947f149ca
Alexandra Vtyurina <sasha.vtyurina@gmail.com>
2014-03-07 23:52:30 +0400

    add proj

---
+++ TochkaK/TochkaK/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TochkaK
+//
+//  Created by Alexandra Vtyurina on 07/03/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LibraryAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LibraryAppDelegate class]));
+    }
+}
